327f9e17d25c411a24a0ebd6ea5548605ee85a91
Artsem Filatau <filatau@adani.by>
2020-10-05 23:09:38 +0300

    cars

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,11 +1,18 @@
 package com.company;
 
 public class Car {
+
+    static int var = 100;
     int speed;
     int maxSpeed;
     int weight;
     int maxWeight;
     String color;
+
+    public Car()
+    {
+        System.out.println("New car created");
+    }
 
     public void addWeight(int w)
     {
--- src/com/company/CarMain.java
+++ src/com/company/CarMain.java
@@ -3,6 +3,7 @@ package com.company;
 public class CarMain {
     public static void main(String[] args) {
         Car car = new Car();
+
         car.color = "Black";
         car.maxWeight = 1000;
         car.maxSpeed = 150;
@@ -10,7 +11,14 @@ public class CarMain {
         car.drive(10);
         car.addWeight(500);
         car.drive(70);
-        car.addWeight(700);
-        car.drive(50);
+
+        System.out.println(Car.var);
+        car.var = 120;
+        System.out.println(Car.var);
+        car.var = 150;
+        System.out.println(car.var);
+
+        Car car1 = new Car();
+        System.out.println(car1.var);
     }
 }
